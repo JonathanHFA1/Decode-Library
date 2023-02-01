bb78277090676fd1852921368402e655b45aa3c2
Jonathan <jonhenrilu@gmail.com>
2023-02-01 12:08:11 +0000

    add nav bar to pages

---
+++ src/Livros/script.js
@@ -0,0 +1,14 @@
+let menuOpen = false;
+const button = document.querySelector("nav ul.button");
+
+const menu = document.querySelector("nav ul.menu");
+
+button.addEventListener("click", function () {
+  if (!menuOpen) {
+    menu.style.display = "flex";
+    menuOpen = true;
+  } else {
+    menu.style.display = "none";
+    menuOpen = false;
+  }
+});
