410b72819e267ccb2d1e3b2ede9ef365b0ab0e99
Jonathan <jonhenrilu@gmail.com>
2023-02-01 12:00:33 +0000

    nav testes

--- src/Register/script.js
+++ src/Register/script.js
@@ -1,14 +1,1 @@
-let menuOpen = false;
-const button = document.querySelector("nav ul.button");
 
-const menu = document.querySelector("nav ul.menu");
-
-button.addEventListener("click", () => {
-  if (!menuOpen) {
-    menu.style.display = "flex";
-    menuOpen = true;
-  } else {
-    menu.style.display = "none";
-    menuOpen = false;
-  }
-});
