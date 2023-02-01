676e92ac1801376d702285a262717752acc0cfa8
Jonathan <jonhenrilu@gmail.com>
2023-02-01 11:17:14 +0000

    add nav bar to register and login

--- src/nav/script.js
+++ src/nav/script.js
@@ -3,7 +3,7 @@ const button = document.querySelector("nav ul.button");
 
 const menu = document.querySelector("nav ul.menu");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", function () {
   if (!menuOpen) {
     menu.style.display = "flex";
     menuOpen = true;
